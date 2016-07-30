5a930ad256e91e720eb50086dbf2b7191e49a3b2
Brent Daugherty <daughertyb@cargotel.com>
2016-07-30 13:10:31 +0200

    update to Ionic beta 10, Angular 2 rc 3

--- app/providers/car/car.ts
+++ app/providers/car/car.ts
@@ -1,4 +1,4 @@
-import {Injectable} from 'angular2/core';
+import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Rx';
 import {SimulateService} from '../simulate/simulate';
 import 'rxjs/add/operator/map';
